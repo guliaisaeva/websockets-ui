6ada7c27bdb9ae492607d86b66457a10c247b3ce
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-27 22:03:55 +0300

    feat: add  configs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js';
+import * as dotenv from 'dotenv';
 
-const HTTP_PORT = 8181;
+dotenv.config();
+
+const HTTP_PORT = process.env.HTTP_PORT || 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
